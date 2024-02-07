b7e173d8c20a8c57b3ea22c62cf193fc43476b33
sofyaeli <sofiteranz@hotmail.com>
2024-02-06 20:46:39 -0500

    creacion de clases

--- reproductor.js
+++ reproductor.js
@@ -19,3 +19,35 @@ boton.addEventListener('click', function () {
     lista.innerHTML = '<li>No se encontraron canciones</li>';
   }
 });
+
+class Song{
+  constructor(nombre, autor, duración, album, año, género, cover, urlSong) {
+    this.nombre=nombre;
+  this.autor=autor;
+  this.duración=duración;
+  this.album=album;
+  this.año=año;
+  this.género=género;
+  this.cover=cover;
+  this.urlSong=urlSong;
+  }
+  
+ 
+  getSongNameAndAlbum() {
+    return '${this.nombre} - ${this.autor} - ${this.duracion} - ${this.album}- ${this.genero}- ${this.año}'
+      }
+
+  }
+
+class CatalogoDeCAnciones {
+  constructor () {
+    this.CatalogoDeCAnciones=[
+      new Song ("cancion1","autor1", "duracion1", "album1", "genero1", "año1", '.album/1album.jpg','.canciones/1.mp3', )
+
+    ]
+  }
+}
+
+
+
+
