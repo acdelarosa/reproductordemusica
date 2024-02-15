491fb93b1dff34c8992f08a3c2f8c26293b7f129
acdelarosa <andlr31@hotmail.com>
2024-02-15 18:40:03 -0500

    log out y log in

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 const usuarios = {
-    "users":[
+    "users": [
         {
             "username": "admin",
             "password": "contrasena1"
@@ -15,26 +15,45 @@ const usuarios = {
 
 const btnLogin = document.getElementById('btnLogin')
 
-btnLogin.addEventListener('click',function(){
+btnLogin.addEventListener('click', function () {
+    event.preventDefault()
+
     const username = document.getElementById('userInput').value;
     const password = document.getElementById('passwordInput').value;
 
     // Utilizamos el método find sobre el array de usuarios para buscar un usuario que coincida.
     // El método find itera sobre cada elemento del array y retorna el primero que cumpla la condición.
-    const user =usuarios.users.find(u => u.username === username && u.password === password);
+    const user = usuarios.users.find(u => u.username === username && u.password === password);
 
 
-     // Comprobamos si la constante user tiene un valor (lo que significa que el usuario fue encontrado).
-    if(user){
 
-         // Usuario encontrado y contraseña correcta
-         window.location.href = "../paginareproductor.html";   // Redirecciona a la paginareproductor.html
-    }else{
+    // Comprobamos si la constante user tiene un valor (lo que significa que el usuario fue encontrado).
+    if (user) {
+        alert('Login successful')
+        localStorage.setItem('isLogged', true)
+        // Usuario encontrado y contraseña correcta
+        window.location.href = "../paginareproductor.html";   // Redirecciona a la paginareproductor.html
+    } else {
         // Usuario no encontrado o contraseña incorrecta
-        alert('Usuario o contraseña incorrecta.');
+        /*  usernameInput.value = ''
+         passwordInput.value = '' */
+        alert('Wrong credentials')
     }
-
-
-
-
 })
+if (localStorage.getItem('isLogged')) {
+    const form = document.getElementById('loginForm')
+    form.innerHTML = `
+    <div class="log">
+
+    <button id="logout">Log out</button>
+          <a href="../paginareproductor.html" id="back">
+           Back
+        </a>
+        </div>`
+
+    const logoutBtn = document.getElementById('logout')
+    logoutBtn.addEventListener('click', () => {
+        localStorage.removeItem('isLogged')
+        window.location.href = '../paginareproductor.html'
+    })
+}
